a52352e96ec01de007e5c1aba4a580d687864b67
Margo <margarita.cortizas@gmail.com>
2024-05-10 00:14:27 -0300

    Entrega Viernes

--- project/Level.gd
+++ project/Level.gd
@@ -1,9 +1,11 @@
 extends Node
 class_name Level
+
 @onready var realizado = $Logros/Realizado
 @onready var no_realizado = $Logros/NoRealizado
 @onready var victory = $Victory
 @onready var adelante = $Adelante
+@onready var ensenanza = $Ensenanza
 
 signal return_requested()
 # Reinicia el nivel
@@ -15,18 +17,13 @@ var is_dragging = false
 
 func _ready():
 	victory.hide()
-	
-	
-func _process(delta):
-	pass
+	adelante.disabled = true
 
 func _on_atrás_pressed():
 	get_tree().change_scene_to_file("res://intro.tscn")
 
 func _victory():
 	victory.show()
-	adelante.disabled = false
-	
 
 func _on_adelante_pressed():
 	print("Ganaste!")
--- project/PlayerCambiante.gd
+++ project/PlayerCambiante.gd
@@ -4,8 +4,12 @@ var texture_hambriento : CompressedTexture2D
 var texture_semienfermo : CompressedTexture2D
 var texture_bastanteenfermo : CompressedTexture2D
 var texture_muyenfermo : CompressedTexture2D
+
 @onready var plato = $"../Plato"
 @onready var anim = $AnimatedSprite2D
+@onready var adelante = $"../Adelante"
+@onready var ensenanza = $"../Ensenanza"
+
 var tipo: String
 var bien: bool = false
 
@@ -13,13 +17,7 @@ func _ready():
 	anim.play("cagadodehambre")
 	tipo = "celiaco"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#
-	#elif plato.elementos.has("Gluten") && plato.elementos.has("Arroz"):
-			#anim.play("masmochito")
-	#elif (plato.elementos.has("Gluten") || plato.elementos.has("Cebada")) || plato.elementos.has("Avena") :
-			#anim.play("mochito")
 	var coincidencias = plato.elementos.count("celiaco")
 	if plato.elementos.is_empty():
 		anim.play("cagadodehambre")
@@ -28,6 +26,8 @@ func _process(delta):
 	elif plato.cantAlimentos == 3 && coincidencias == 0:
 		get_tree().root.get_child(0)._victory()
 		bien = true
+		adelante.disabled = false
+		ensenanza.show()
 		anim.play("recontento")
 	elif coincidencias == 2:
 		anim.play("masmochito")
