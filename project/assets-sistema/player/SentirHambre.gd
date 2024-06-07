6109c8058eb8bb48a471141944726cb6cff378b6
Margo <margarita.cortizas@gmail.com>
2024-06-06 23:59:30 -0300

    Ultimos cambios

--- project/assets-sistema/player/SentirHambre.gd
+++ project/assets-sistema/player/SentirHambre.gd
@@ -5,7 +5,7 @@ extends "res://assets-sistema/player/AbstractState.gd"
 
 func entra_item_plato(item, player):
 	super.entra_item_plato(item, player)
-	print(player.plato.cantAlimentos)
 	if item.esPositivo && player.plato.cantAlimentos == (player.manager_level.level_resource.cantidadPositivos -1):
 		level._victory()
+		animation_name = "recontento"
 		hambre.hide()
