8b93c52eb4849fa265307848bebfe9b6de0c1275
Margo <margarita.cortizas@gmail.com>
2024-06-21 19:28:31 -0300

    Niveles configurables

--- project/niveles/global.gd
+++ project/niveles/global.gd
@@ -1,7 +1,16 @@
 extends Node
 
-var is_dragging : Object
 
-enum Condicion {
-	KETO, CELIACO, VEGANO, DIABETICO, VEGETARIANO
-}
+const DESAYUNO = preload("res://assets-sistema/interfaz/desayuno.png")
+const ALMUERZO = preload("res://assets-sistema/interfaz/almuerzo.png")
+const MERIENDA = preload("res://assets-sistema/interfaz/merienda.png")
+const CENA = preload("res://assets-sistema/interfaz/cena.png")
+
+const PREPARA_CELIAQUIA = preload("res://assets-sistema/interfaz/prepara-celiaquia.png")
+
+var is_dragging : Object
+var manager_level 
+var current_level = 1
+var items_level = {	1: [1,1, ALMUERZO, PREPARA_CELIAQUIA, Ensenanzas.ENSENANZA_CELIAQUIA_1], 
+					2: [2,2, DESAYUNO, PREPARA_CELIAQUIA,Ensenanzas.ENSENANZA_CELIAQUIA_2],
+					3: [3,3, CENA, PREPARA_CELIAQUIA, Ensenanzas.ENSENANZA_CELIAQUIA_3]}
--- project/niveles/libro.gd
+++ project/niveles/libro.gd
@@ -1,32 +1,19 @@
 extends Node2D
 
-var LEVEL_1
-var itemsPositivos 
-var itemsNegativos
-var comida
-var condicion
-var cantItemsPositivos
-var cantItemsNegativos
-var ruta
-
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file(ruta)
+	get_tree().change_scene_to_file("res://niveles/nivel_1/Level.tscn")
 
 func _on_cap_1_pressed():
-	
-	LEVEL_1.cantidadNegativos = 1
-	LEVEL_1.cantidadPositivos = 1
-	LEVEL_1.itemsNegativos = preload("res://items/fideos.tres").instanciate()
-	LEVEL_1.itemsPositivos = preload("res://items/fideosarroz.tres").instanciate()
-	LEVEL_1.comida = null
-	LEVEL_1.condicion = null
-	get_tree().change_scene_to_file(LEVEL_1.instanciate())
+	Global.current_level = 1
+	get_tree().change_scene_to_file("res://niveles/nivel_1/Level.tscn")
 	
 func _on_cap_2_pressed():
+	Global.current_level = 2
 	get_tree().change_scene_to_file("res://niveles/nivel_1/Level.tscn")
 
 func _on_cap_3_pressed():
+	Global.current_level = 3
 	get_tree().change_scene_to_file("res://niveles/nivel_1/Level.tscn")
 
 func _on_cap_4_pressed():
