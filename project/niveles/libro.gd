cc21610cb979c78a17bf4fa88501bc8abaf99798
gonzabender <gonzabender@gmail.com>
2024-06-26 16:55:43 -0300

    Ir para atras

--- project/niveles/libro.gd
+++ project/niveles/libro.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://niveles/libro.tscn")
+	get_tree().change_scene_to_file("res://niveles/intro.tscn")
 
 func _on_cap_1_pressed():
 	Global.current_level = 1
